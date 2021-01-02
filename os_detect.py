3dd4ea8b614ea9398e97238145b2dbc973566fad
Billy <billy.codes@gmail.com>
2021-01-02 22:51:36 +0800

    Updated

--- os_detect.py
+++ os_detect.py
@@ -1,9 +1,4 @@
 # Getting OS installed using sys.platform module
 from sys import platform
 
-if platform == "linux" or platform == "linux2":
-    print("Linux")
-elif platform == "darwin":
-    print("Mac OS X")
-elif platform == "win32":
-	print("Windows")
+print(platform)
