809c948ebabf1d5421fc94d938f268a6bee168d7
Billy <billy.codes@gmail.com>
2021-01-06 13:15:04 +0800

    Added new modules

--- os_detect.py
+++ os_detect.py
@@ -1,4 +1,22 @@
-# Getting OS installed using sys.platform module
-from sys import platform
+# Getting OS installed using sys.platform and os module
+# Use samples from this script to detect your OS
 
-print(platform)
+##### Documentation #####
+# Platform module has a finer granularity
+# Platform	: https://docs.python.org/3/library/platform.html
+# OS 		: https://docs.python.org/3/library/os.html
+
+
+import platform, os
+
+def using_os():
+	if os.name == 'nt':
+		print('Windows')
+	else:
+		print('Linux')
+
+def using_platform():
+	print(platform.system())	
+
+using_os()
+using_platform()
