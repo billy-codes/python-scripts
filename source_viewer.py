8ab4ea3282177a5139d7167efb0b442c1f8d7761
Billy <billy.codes@gmail.com>
2021-01-11 14:54:43 +0800

    Added a timer and output to text file function

--- source_viewer.py
+++ source_viewer.py
@@ -1,28 +1,37 @@
 # Use this script to grab source codes from Web Pages
 # Using either urllib2 module or request module
+
 # Future Features: 
 #	- Using Beautiful module to parse source codes and results
 #	- Command-line interface for parsing arguments
+#	- Validation for target URL
 
 # Current Feature:
 #	- Grab Web Page source code
 #	- Save results into a text file
 #	- Get current working directory
+#	- Calculate Time Taken
 
 # Programming Principles
 #	- File Handling
 #	- Using a module and manipulating object results
 #	- Custom User input
 
-import urllib2
-import request
+from urllib.request import urlopen
 import os
+import time
 
 site = input("Enter target URL: ")
-response = urllib2.urlopen(site)
+
+timeStart = time.time()
+response = urlopen(site)
 source_code = response.read()	# response object
 
-output_file = open("results.txt", "w")
+output_file = open("results.txt", "wb")
 output_file.write(source_code)
-output_directory = os.getcwd() + "results.txt"
+output_directory = os.getcwd() + "\\results.txt"
 print("Output saved into: ", output_directory)
+
+timeEnd = time.time()
+totalTime = timeEnd - timeStart
+print("Total time: ", round(totalTime,2), "(s)")
