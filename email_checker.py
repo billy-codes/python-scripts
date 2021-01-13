4a701705954a51e705c65d5553b2ee85cad54c4d
Billy <billy.codes@gmail.com>
2021-01-13 23:32:23 +0800

    Update script and corrected errors

--- email_checker.py
+++ email_checker.py
@@ -1,6 +1,13 @@
+#	A simple email format validator using regex
+#	Future Features:
+#		- Email user validator (Email Status)
+#		- Check whether email is professional or commercial (Email Type)
+#		- Check whether email host is up or offline (Server Status)
+#
+#	Current Features:
+#		- Email format validation
+
 import re
-import urllib.request
-from smtplib import SMTP
 
 # use partition() or split() function
 def get_user(email):
@@ -19,25 +26,9 @@ def validate_email(email):
 	if(re.search(regex,email)):
 		return True
 
-def is_online(domain):
-	status = urllib.request.urlopen(domain).getcode()
-	if status == 200:
-		return True
-
 email = "intan@fiske.com.my"
-port = 25
-mail_server = "mail." + get_domain(email)
 
-try:
-    with SMTP(mail_server, port) as smtp:
-        result = smtp.verify(email)
-        print("SMTP CODE: {} \nMESSAGE: {}".format(*result))
-except:
-    	print("Error")
-
-
-# smtpTest = SMTP(mail_server, port)
-# result = s
-# print(mail_server)
-# if(validate_email(email)):
-# 	print("Valid Email Format")
+if(validate_email(email)):
+	print("Email: ", email, " is valid")
+else:
+	print("Email: ", email, " is invalid")
