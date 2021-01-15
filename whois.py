7667d8005faf3c78ad8c3cb07c969a33406df661
Billy <billy.codes@gmail.com>
2021-01-15 17:05:30 +0800

    Initial commit for WHOIS tool

--- whois.py
+++ whois.py
@@ -1,2 +1,37 @@
 # A simple WHOIS tool by parsing HTML
 
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
+
+whois_website = "https://who.is/whois/"
+
+# domain = input("Enter target domain: ")
+domain = "msn.com"
+domain = domain.replace("www.","").replace("https://","").replace("http://","")
+target = whois_website + domain
+
+url_client = urlopen(target)
+data_source = bs(url_client.read(), "html.parser")
+url_client.close()
+
+response_headers = data_source.findAll("div", {"class": "col-md-12 queryResponseHeader"})
+registrar_info_header = response_headers[0].text
+important_dates_header = response_headers[1].text
+similar_domains_header = response_headers[2].text
+registrar_data_header = response_headers[3].text
+
+response_body = data_source.findAll("div", {"class": "col-md-12 queryResponseBody"})
+registrar_info_body = response_body[0].text
+important_dates_body = response_body[1].text
+similar_domains_body = response_body[3].text
+
+registrar_data_body = data_source.findAll("div", {"class": "col-md-12 queryResponseBodyValue"})
+registrar_data_body = registrar_data_body[1].findAll("div", {"class": "row"})
+
+
+for i in range(0, len(registrar_data_body)):
+	r_label = registrar_data_body[i].findAll("strong")
+	r_desc = registrar_data_body[i].findAll("div", {"class","col-md-7"})
+	print(r_label[0].text,": ",r_desc[0].text)
+
+# print(registrar_data_body[0])
