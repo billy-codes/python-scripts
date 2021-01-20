0f1870afafa1ecf53f805261cc7697bb942cab64
Billy <billy.codes@gmail.com>
2021-01-20 17:00:31 +0800

    Completed script

--- geoip_resolver.py
+++ geoip_resolver.py
@@ -1,35 +1,47 @@
-# A simple script to resolve the IP address's country
+# A simple script to resolve the IP address's country using ipapo.co
 
 # Features:
-#	- Submits POST requests using requests.post()
-#	- Bypass webscraping blockage
+#	- Bypass anti-scraping using headers
 #	- Grabs GeoIP location from 4 different sources
 
-from urllib import request, parse
+# Future Features
+#	[ ] Resolve multiple IP Addresses
+#	[ ] Save results to text file
+# 	[ ] Validate IP Address before tracing
+#	[ ] Error handling
+
+from urllib import request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
+import argparse
+import time
 
-url = "https://www.whatismyip.com/wp-content/themes/understrap-wimi/inc/response.php"
-ip_address = "175.136.205.184"
+parser = argparse.ArgumentParser(
+		description = "Resolve IP Address Geolocation",
+		usage = "geoip_resolver.py -ip 127.0.0.1",
+		epilog = "Author: Billy Khaled, billy.codes@gmail.com")
 
-header = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-			'accept-encoding': 'gzip, deflate, br',
-			'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8',
-			'cache-control': 'max-age=0',
-			'content-type': 'application/x-www-form-urlencoded',
-			'origin': 'https://www.whatismyip.com',
-			'referer': 'https://www.whatismyip.com/ip-address-lookup/',
-			'sec-fetch-dest': 'empty',
-			'sec-fetch-mode': 'cors',
-			'sec-fetch-site': 'same-origin',
-			'x-requested-with': 'XMLHttpRequest',
-			'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
-}
-post_data = {"ip" : ip_address, "action" : "ip-lookup"}
+parser.add_argument("-ip", "--ipaddress", metavar='', required=True, help="Trace IP")
+args, unknown = parser.parse_known_args()
 
-data = parse.urlencode(post_data).encode('ascii')
-req = request.Request(url,data = data,  headers = header)
-response = request.urlopen(req)
-source_code = response.read()
+if __name__ == '__main__':
+	timeStart = time.time()
+	print("Tracing IP Address:")
+	url = "https://ipapi.co/"
+	full_url = url + args.ipaddress
+	header = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',}
 
-print(source_code)
+	# ROR methodology = Request, Open, Read
+	req = request.Request(full_url, headers = header)
+	response = request.urlopen(req)
+	source_code = soup(response.read(), "html.parser")
+	table_row = source_code.findAll("tr", {"class" : "ip-striped"})
+
+	print("########## RESULTS ##########")
+	for i in range(0, len(table_row)):
+		table_data = table_row[i].findAll("td")
+		if not table_data[0].text: # if data is empty
+			continue
+		results = table_data[0].text.strip() + " : " + table_data[1].text.strip()
+		print(results)
+print("Total Time: ", round(time.time() - timeStart,2), "(s)")
