4387761a65dc6d2020195d389506d46284fb9abe
Billy <billy.codes@gmail.com>
2021-01-30 18:48:03 +0800

    Completed script

--- password_generator.py
+++ password_generator.py
@@ -2,5 +2,18 @@
 
 import string
 import random
+import argparse
 
+parser = argparse.ArgumentParser(
+    description = "A simple password generator",
+    usage = "password_generator.py -c 6",
+    epilog = "Author: Billy Khaled, billy.codes@gmail.com"
+)
 
+parser.add_argument("-c", "--characters", metavar='', required=True, help="Generate with number of characters")
+args, unknown = parser.parse_known_args()
+
+if __name__ == '__main__':
+    if(args.characters and args.characters.isnumeric()):
+        password = ''.join(random.choices(string.ascii_letters + string.digits + string.ascii_lowercase + string.ascii_uppercase + string.punctuation, k=int(args.characters)))
+        print("Generated Password: ",password)
