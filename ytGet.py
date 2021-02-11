a913ec52f30d156a50731ca98307fb1ecb259acb
Billy <billy.codes@gmail.com>
2021-02-11 23:43:59 +0800

    Adding features

--- ytGet.py
+++ ytGet.py
@@ -7,5 +7,9 @@ from pytube import YouTube
 url = "https://www.youtube.com/watch?v=hGIW2fDb0jg&t=1837s"
 
 yt_obj = YouTube(url)
+
+yt_Title = yt_obj.title
+yt_Duration = yt_obj.length / 60 # in minutes
+yt_Views = yt_obj.views
 for stream in yt_obj.streams:
-    print(stream)
+    print(stream.itag)
