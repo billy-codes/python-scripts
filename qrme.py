26222c224c5c404b952da22a800275d5b40e1bae
Billy <billy.codes@gmail.com>
2021-03-01 10:35:32 +0800

    Completed Script

--- qrme.py
+++ qrme.py
@@ -3,12 +3,17 @@
 # using the qrcode module 
 
 # User inputs can include links or texts
+# We need to install Pillow module to generate image
 # First Install: pip3 install qrcode
+# Second Install: pip3 install image 
 
 #!/usr/bin/python
 
 import qrcode
 import argparse
+import image
+import string
+import random
 
 # Setup parser
 parser = argparse.ArgumentParser(
@@ -21,4 +26,18 @@ args, unknown = parser.parse_known_args()
 
 if __name__ == '__main__':
     data = args.qrcode
-    image = qrcode.make(data)
+    # customize QR Code by creating an object
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border = 4,
+    )
+    if(data):
+        title = "".join(random.choices(string.ascii_uppercase+string.ascii_lowercase, k = 5))
+        qr.add_data(data)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color = "black", back_color = "white")
+        qr_title = title + "_QR.png"
+        img.save(qr_title)
+        print("Successfully Generated QR Code")
