258bf662b36983c50807902ce0e6f729a674512b
Billy <billy.codes@gmail.com>
2021-03-12 00:59:04 +0800

    Added Availability() function

--- instaUsername.py
+++ instaUsername.py
@@ -26,3 +26,19 @@ def validate(username):
     if(re.search(regex,username)):
         return True
 
+def availability(username):
+    # use instausername.com to check for availability
+    url = "https://instausername.com/availability?q="
+    instaProfile = url + username
+
+    opener = urllib.request.build_opener()
+    opener. addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)')]
+    urllib.request.install_opener(opener)
+    client = urlopen(instaProfile)
+    sourceCode = bs(client.read(), "html.parser")
+    client.close()
+    
+    response = sourceCode.findAll("div", {"class","frm--msg sccs"})
+    if "free" in str(response): 
+        return True
+
