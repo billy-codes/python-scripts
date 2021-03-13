3ced88f841441d30b43fa57a238afc9497d82e35
Billy <billy.codes@gmail.com>
2021-03-13 12:17:14 +0800

    Added hashed() function

--- cyptography/md5crack/md5crack.py
+++ cyptography/md5crack/md5crack.py
@@ -12,17 +12,20 @@ import time
 
 parser = argparse.ArgumentParser(
     description = "Simple MD5 Hash Cracker",
-    usage = os.path.basename(__file__) + " -h <MD5 Hash>",
+    usage = os.path.basename(__file__) + " -h <MD5 Hash> -w <Wordlist.txt>",
     epilog = "Author: Billy Khaled, billy.codes@gmail.com"
 )
 
-#parser.add_argument("-h", "--hash", metavar='', help="Enter MD5 Hash to Crack")
+# parser.add_argument("-m", "--md5", metavar='', required=True, help="Enter MD5 Hash to Crack")
 parser.add_argument("-w", "--wordlist", metavar='', required=True, help="Wordlist File in .txt")
 
 args, unknown = parser.parse_known_args()
 
-#userHash = args.hash
+# userHash = args.hash
 wordlist = args.wordlist
+
+def hashed(string):
+    return hashlib.md5(string.encode()).hexdigest()
 
 try:
     file1 = open(wordlist, 'r')
@@ -31,6 +34,6 @@ except:
     print("Error Reading Wordlist")
 
 for line in lines:
-    print(line)
+    print(hashed(line))
 
 
