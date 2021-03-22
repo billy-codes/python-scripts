cfbdbe65fce05b0eaef18e5e7ae00196dc35ef5c
Billy <billy.codes@gmail.com>
2021-03-22 21:38:53 +0800

    Added a time feature

--- speed.py
+++ speed.py
@@ -6,10 +6,15 @@
 # pip3 install speedtest-cli
 
 import speedtest
+import time
 
-# create instance of Speedtest
+# initialization
 s = speedtest.Speedtest()
+timeStart = time.time()
 
+print("Testing Speed...")
 
 print("Download Speed: ", s.download())
 print("Upload Speed: ", s.upload())
+
+print("Total Time: ", round(time.time() - timeStart,2),"(s)")
