0af88c41a2f7e2dda3943988c095736962553959
Billy <billy.codes@gmail.com>
2021-03-29 18:40:21 +0800

    Completed Script

--- math/bmiCalculator.py
+++ math/bmiCalculator.py
@@ -4,4 +4,17 @@ height = float(input("Enter Height (cm): "))
 weight = float(input("Enter Weight (kg): "))
 
 # BMI = weight (kg) / (height (m))^2
-bmi = weight / (height/100)^2
+bmi = weight / (height/100)**2
+
+healthStatus = "None"
+if bmi < 18.4:
+    healthStatus = "Underweight"
+elif (bmi > 18.4) and (bmi < 24.9):
+    healthStatus = "Normal Weight"
+elif (bmi >= 25) and (bmi <= 29.9):
+    healtStatus = "Over Weight"
+elif (bmi > 29.9):
+    healthStatus = "Obese"
+
+print(f"BMI: {round(bmi,2)}")
+print(f"Health Status: {healthStatus}")
